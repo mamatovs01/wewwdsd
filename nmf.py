752cb04324397d1f5152574f77170e3d11587aa4
username <email@gmail.com>
2024-11-16 15:53:45 +0600

    Your commit

--- nmf.py
+++ nmf.py
@@ -1,22 +1,29 @@
-import threading as th
+# import threading as th
 
-def count(end):
-    for i in range(end):
-        print(i)
-    print(f"Завершено {end}")
+# def count(end):
+#     for i in range(end):
+#         print(i)
+#     print(f"Завершено {end}")
 
-def letters(word):
-    for i in word:
-        print(i)
-    print(f"{word}  завершено")
+# def letters(word):
+#     for i in word:
+#         print(i)
+#     print(f"{word}  завершено")
 
-thread1 = th.Thread(target=count,args=(15,))
-thread2 = th.Thread(target=letters,args=("hello world!!!",))
+# thread1 = th.Thread(target=count,args=(15,))
+# thread2 = th.Thread(target=letters,args=("hello world!!!",))
 
-thread1.start()
-thread2.start()
+# thread1.start()
+# thread2.start()
 
-thread1.join()
-thread2.join()
+# thread1.join()
+# thread2.join()
 
 
+class Character:
+    def __init__(self, xp, st, mp):
+        self.xp = xp
+        self.st = st
+        self.mp = mp 
+    def getInfo(self):
+        return f"xp = {self.xp} st = {self.st } mp = {self.mp}"
